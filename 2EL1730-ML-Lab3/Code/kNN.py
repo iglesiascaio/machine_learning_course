cefd83fbf6a6ce74b2bc6ae25fbaa815f2adb87e
iglesiascaio <caio.iglesias@usp.br>
2020-12-11 18:18:15 +0100

    TD 3

---
+++ 2EL1730-ML-Lab3/Code/kNN.py
@@ -0,0 +1,39 @@
+import numpy as np
+from statistics import mode
+
+def kNN(k, X, labels, y):
+    # Assigns to the test instance the label of the majority of the labels of the k closest 
+	# training examples using the kNN with euclidean distance.
+    #
+    # Input: k: number of nearest neighbors
+    #        X: training data           
+    #        labels: class labels of training data
+    #        y: test data
+    
+    
+    # ====================== ADD YOUR CODE HERE =============================
+    # Instructions: Run the kNN algorithm to predict the class of
+    #               y. Rows of X correspond to observations, columns
+    #               to features. The 'labels' vector contains the 
+    #               class to which each observation of the training 
+    #               data X belongs. Calculate the distance betweet y and each 
+    #               row of X, find  the k closest observations and give y 
+    #               the class of the majority of them.
+    #
+    # Note: To compute the distance betweet two vectors A and B use
+    #       use the np.linalg.norm() function.
+    #
+    
+    
+    distances = np.linalg.norm(X-y, axis=1)
+    idx = distances.argsort()
+    label = mode(labels[idx[:k]])
+    
+    
+    
+            
+       
+
+    # return the label of the test data
+    
+    return label
